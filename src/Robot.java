352e3ee51a14b631b02d9432e833a75c86d732ab
Matt <mattnappo@gmail.com>
2019-10-24 13:53:14 -0400

    Adding a robot bass/abstraction class.

---
+++ src/Robot.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import java.lang.Math;
+
+@TeleOp(name="Robot", group="Iterative Opmode")
+public class Robot extends OpMode {
+    
+}
